548b93b4797ec1c687d26870f293b937caf26c50
Anish Mainali <anishmainali31@gmail.com>
2024-08-16 12:56:37 +0545

    authcontrollers . for every more controller

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const {home, register} = require("../controllers/auth-controller");
+const authcontrollers = require("../controllers/auth-controller");
 
 
 
-router.route("/").get(home);
+router.route("/").get(authcontrollers.home);
 
-router.route("/register").get(register)
+router.route("/register").get(authcontrollers.register);
 
 
 
